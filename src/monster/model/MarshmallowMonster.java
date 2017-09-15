2766f6623964440973d5772e5a177498c3e9448e
Dustin Schimel <dustin.schimel@gmail.com>
2017-09-15 12:11:34 -0600

    Added toString about the MarshmallowMonster

--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -2,5 +2,34 @@ package monster.model;
 
 public class MarshmallowMonster
 {
-
+	//Declaration section of data members
+	
+	private String name;
+	private int eyeCount;
+	private int armCount;
+	private int tentacleAmount;
+	private boolean hasBloop;
+	
+	public MarshmallowMonster()
+	{
+		//initilizes everything to 0, false or null.
+	}
+	
+	public MarshmallowMonster(String name, int eyeCount, int arms, int tentacleCount, boolean hasBloop)
+	{
+		this.name = name;
+		this.eyeCount = eyeCount;
+		this.armCount = arms;
+		this.tentacleAmount = tentacleCount;
+		this.hasBloop = hasBloop;
+	}
+	
+	public String toString()
+	{
+		String description = "I am a silly monster, my name is " + name + ", I have " + eyeCount + " eyes and only";
+		description += tentacleAmount + " tentacles but I have " + armCount + " arms!" + hasBloop + " tells you";
+		description += " if I have a bloop";
+		
+		return description;
+	}
 }
