a29069ac400e7c4908b1fe492dd5b155cf14cfad
Dustin Schimel <dustin.schimel@gmail.com>
2017-09-18 11:55:53 -0600

    Added getters and setters

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,6 +1,31 @@
 package monster.controller;
 
+import monster.model.MarshmallowMonster;
+import java.util.Scanner;
+
 public class MonsterController
 {
+	public void start()
+	{
+		MarshmallowMonster basic = new MarshmallowMonster();
+		System.out.println(basic);
+		MarshmallowMonster nelson = new MarshmallowMonster("Nelson", 2, 3, 0, true);
+		System.out.println(nelson);
+		System.out.println("I am feeling hungry, I am going to eat one of Nelson's arms");
+		nelson.setArmCount(nelson.getArmCount() - 1);
+		System.out.println(nelson);
+		
+		interactWithMonster(nelson);
+	}
 	
+	private void interactWithMonster(MarshmallowMonster currentMonster)
+	{
+		Scanner myScanner = new Scanner(System.in);
+		System.out.println(currentMonster.getName() + " wants to know how many eyes you want to eat, please type in how many");
+		int consumed = myScanner.nextInt();
+		currentMonster.setEyeCount(currentMonster.getEyeCount() - consumed);
+		System.out.println(currentMonster);
+		
+		myScanner.close();
+	}
 }
--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -7,7 +7,7 @@ public class MarshmallowMonster
 	private String name;
 	private int eyeCount;
 	private int armCount;
-	private int tentacleAmount;
+	private double tentacleAmount;
 	private boolean hasBloop;
 	
 	public MarshmallowMonster()
@@ -15,7 +15,7 @@ public class MarshmallowMonster
 		//initilizes everything to 0, false or null.
 	}
 	
-	public MarshmallowMonster(String name, int eyeCount, int arms, int tentacleCount, boolean hasBloop)
+	public MarshmallowMonster(String name, int eyeCount, int arms, double tentacleCount, boolean hasBloop)
 	{
 		this.name = name;
 		this.eyeCount = eyeCount;
@@ -24,9 +24,59 @@ public class MarshmallowMonster
 		this.hasBloop = hasBloop;
 	}
 	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public int getEyeCount()
+	{
+		return eyeCount;
+	}
+	
+	public int getArmCount()
+	{
+		return armCount;
+	}
+	
+	public double getTentacleAmount()
+	{
+		return tentacleAmount;
+	}
+	
+	public boolean hasBloop()
+	{
+		return hasBloop;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setEyeCount(int eyeCount)
+	{
+		this.eyeCount = eyeCount;
+	}
+	
+	public void setArmCount(int armCount)
+	{
+		this.armCount = armCount;
+	}
+	
+	public void setTentacleAmount(double tentacleAmount)
+	{
+		this.tentacleAmount = tentacleAmount;
+	}
+	
+	public void setBloop(boolean hasBloop)
+	{
+		this.hasBloop = hasBloop;
+	}
+	
 	public String toString()
 	{
-		String description = "I am a silly monster, my name is " + name + ", I have " + eyeCount + " eyes and only";
+		String description = "I am a silly monster, my name is " + name + ", I have " + eyeCount + " eyes and ";
 		description += tentacleAmount + " tentacles but I have " + armCount + " arms!" + hasBloop + " tells you";
 		description += " if I have a bloop";
 		
